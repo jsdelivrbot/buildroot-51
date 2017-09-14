d24d573c7386447ca5941cb82698f1ee9e72c2a6
Laboratório de Redes <andre.antonitsch@acad.pucrs.br>
2017-09-14 12:18:54 -0300

    Syscall test

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -9,4 +9,7 @@ chmod +x $BASE_DIR/target/etc/init.d/Sweb_server
 cp -rf $BASE_DIR/../custom-scripts/serverscripts $BASE_DIR/target/usr/bin
 chmod +xr $BASE_DIR/target/usr/bin/serverscripts
 
-
+#Compile the syscall_test.c
+BUILDROOT_DIR=$BASE_DIR/..
+COMPILER=$BUILDROOT_DIR/output/host/bin/i686-buildroot-linux-uclibc-gcc
+$COMPILER -o $BUILDROOT_DIR/output/target/bin/syscall_test $BUILDROOT_DIR/custom-scripts/syscall_test.c
