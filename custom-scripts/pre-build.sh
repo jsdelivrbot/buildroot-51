0ba3851491f7597afb63f372a27f8b02d527fa30
Laboratório de Redes <andre.antonitsch@acad.pucrs.br>
2017-09-14 12:59:55 -0300

    prebuild changes - driver

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -13,3 +13,5 @@ chmod +xr $BASE_DIR/target/usr/bin/serverscripts
 BUILDROOT_DIR=$BASE_DIR/..
 COMPILER=$BUILDROOT_DIR/output/host/bin/i686-buildroot-linux-uclibc-gcc
 $COMPILER -o $BUILDROOT_DIR/output/target/bin/syscall_test $BUILDROOT_DIR/custom-scripts/syscall_test.c
+
+make -C $BASE_DIR/../../hello_world_driver/
