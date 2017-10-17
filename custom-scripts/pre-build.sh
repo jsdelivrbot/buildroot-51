5419a520231fce42a559167a48ddc84612bb85ec
Laboratório de Redes <andre.antonitsch@acad.pucrs.br>
2017-10-17 13:26:12 -0200

    skldjaksld

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -16,12 +16,12 @@ BUILDROOT_DIR=$BASE_DIR/..
 COMPILER=$BUILDROOT_DIR/output/host/bin/i686-buildroot-linux-uclibc-gcc
 $COMPILER -o $BUILDROOT_DIR/output/target/bin/syscall_test $BUILDROOT_DIR/custom-scripts/syscall_test.c
 
-make -C $BASE_DIR/../../hello_world_driver/
+#make -C $BASE_DIR/../../hello_world_driver/
 
 #Compile the disk_access_test.c
-COMPILER=$BUILDROOT_DIR/output/host/bin/i686-buildroot-linux-uclibc-gcc
-$COMPILER -o $BUILDROOT_DIR/output/target/bin/disk_access_test $BUILDROOT_DIR/custom-scripts/disk_access_test.c
+#COMPILER=$BUILDROOT_DIR/output/host/bin/i686-buildroot-linux-uclibc-gcc
+#$COMPILER -o $BUILDROOT_DIR/output/target/bin/disk_access_test $BUILDROOT_DIR/custom-scripts/disk_access_test.c
 
 
-make -C $BASE_DIR/../../LabSisOpT2/
+#make -C $BASE_DIR/../../LabSisOpT2/
 
